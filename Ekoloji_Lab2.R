f26f17c8de0a0ec8e5ca137decf8090dcf1457ef
irremgok06-collab <irrem.gok06@gmail.com>
2026-03-09 22:26:21 +0300

    Update Ekoloji_Lab2.R

--- Ekoloji_Lab2.R
+++ Ekoloji_Lab2.R
@@ -4,13 +4,13 @@ AB_data <- read.csv2("Data/Data_agac.csv",sep = ";",header = TRUE,col.names = c(
 AB_data
 
 # Verileri A ve B alanı olmak üzere iki kısma ayırın ve boş verileri atın (NA)
-alan_A <- AB_data$A
-alan_B <- na.omit(AB_data$B)
+alan_A <- 24.6 25.8 26.1 22.9 25.1 27.3 24.0 24.5 23.9 26.2 24.3 24.6 23.3 25.5 28.1 24.8 23.5 26.3 25.4 25.5 23.9 27.0 24.8 22.9 25.4 24.0 22.9 24.8 22.9 24.0
+alan_B <- 23.5 25.2 21.2 22.7 26.3 22.6 21.4 23.5 24.1 25.6 23.9 23.6 26.3 23.2 25.6 21.3 22.4 24.9 23.8 24.6 24.1 22.6 22.7 25.7 24.3 23.2 22.7 21.4 21.3 23.5 
 
 # Tanımlayıcı istatistik
-summary(alan_A)
+A alanı ve B alanındaki ağaçların ortalama boy uzunlukları arasında anlamlı bir fark yoktur.(alan_A)
 
-summary(alan_B)
+A alanı ve B alanındaki ağaçların ortalama boy uzunlukları arasında anlamlı bir fark vardır.(alan_B)
 
 # standart sapma ve varyasyon
 
